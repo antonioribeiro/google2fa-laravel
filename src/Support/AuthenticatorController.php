e46680ebf8290bba0785874f399065a3d162927b
Alexis Saettler <alexis@saettler.org>
2018-01-09 19:37:08 +0100

    Fix bad line

--- src/Support/AuthenticatorController.php
+++ src/Support/AuthenticatorController.php
@@ -27,7 +27,7 @@ class AuthenticatorController extends Authenticator
      */
     public function __construct(IlluminateRequest $request)
     {
-        $this->boot($request);
+        parent::__construct($request);
     }
 
     /**
@@ -39,7 +39,7 @@ class AuthenticatorController extends Authenticator
      */
     public function boot($request)
     {
-        $this->setRequest($request);
+        parent::boot($request);
         return $this;
     }
 
@@ -72,7 +72,9 @@ class AuthenticatorController extends Authenticator
      */
     public function isAuthenticated()
     {
-        return $this->canPassWithoutCheckingOTP() ?? $this->checkOTP();
+        return $this->canPassWithoutCheckingOTP()
+            ? true
+            : $this->checkOTP();
     }
 
     /**
